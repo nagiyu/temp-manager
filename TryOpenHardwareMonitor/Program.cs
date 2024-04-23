b2c9f728c59388c7d4aca21edbe599f4d7278a7a
nagiyu <otonagiyu@gmail.com>
2024-04-23 23:40:31 +0900

    Updated .NET Framework version in App.config and made various changes across multiple files

---
+++ TryOpenHardwareMonitor/Program.cs
@@ -0,0 +1,38 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryOpenHardwareMonitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var computer = new Computer
+            {
+                CPUEnabled = true,
+                GPUEnabled = true
+            };
+
+            computer.Open();
+
+            foreach (var hardware in computer.Hardware)
+            {
+                hardware.Update();
+                Console.WriteLine("Hardware: " + hardware.Name);
+                foreach (var sensor in hardware.Sensors)
+                {
+                    Console.WriteLine("Type: " + sensor.SensorType + " Sensor: " + sensor.Name + " Value: " + sensor.Value);
+                }
+            }
+
+            computer.Close();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
