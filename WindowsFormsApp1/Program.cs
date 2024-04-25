92bdef00c511f26a35c47251af1f6aa834b9b4c7
nagiyu <otonagiyu@gmail.com>
2024-04-25 23:34:41 +0900

    Update OpenHardwareMonitor submodule and add new classes and files for hardware monitoring functionality

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var computer = new Computer
+            {
+                CPUEnabled = true,
+                GPUEnabled = true
+            };
+
+            computer.Open();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Application.ApplicationExit += (sender, e) => computer.Close();
+        }
+    }
+}
